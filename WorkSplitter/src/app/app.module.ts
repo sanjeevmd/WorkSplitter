79bf60ad5fbba1cddc33d56b7a4245657ff27296
Sanjeev Mahalakshmi Dharanipathy <59342335+sanjeevmd@users.noreply.github.com>
2020-05-16 16:17:41 -0400

    Update app.module.ts

--- WorkSplitter/src/app/app.module.ts
+++ WorkSplitter/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider} from 'angular4-social-login';
 
-const google_oauth_client_id : string = "1044618247563-d00ht16635l5kapf683i9vp7e88kilbu.apps.googleusercontent.com"
+const google_oauth_client_id : string = ""
 
 let config = new AuthServiceConfig([
   {
@@ -33,18 +33,7 @@ export function provideConfig() {
     BrowserAnimationsModule,
     MaterialModule,
     ReactiveFormsModule, CommonModule,
-    // AngularFireModule.initializeApp(
-    //   {
-    //     apiKey: "AIzaSyAgIxzLPtSHc9dbU94TOs6pQw63Cu6gn_E",
-    //     authDomain: "worksplitter-6429c.firebaseapp.com",
-    //     databaseURL: "https://worksplitter-6429c.firebaseio.com",
-    //     projectId: "worksplitter-6429c",
-    //     storageBucket: "worksplitter-6429c.appspot.com",
-    //     messagingSenderId: "1044618247563",
-    //     appId: "1:1044618247563:web:32e03923d23c64b62d438e",
-    //     measurementId: "G-PD9BTN4YLY"
-    //   }
-    // )
+   
   ],
   providers : [{provide : AuthServiceConfig, useFactory: provideConfig}],
   bootstrap: [AppComponent]
